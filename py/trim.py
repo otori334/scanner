0c6312e5e0eb5ae1fa0fc954653adf65ecaea869
otori334 <41427185+otori334@users.noreply.github.com>
2021-04-29 00:08:57 +0900

    Update trim.py
    - rows,cols で指定すると便利な場合がある

--- py/trim.py
+++ py/trim.py
@@ -7,6 +7,8 @@ output_file_path = sys.argv[2]
 
 img = cv2.imread(input_file_path)
 
+rows,cols,channels = img.shape
+
 # 左上
 a = np.array([230, 175])
 # 右下
@@ -14,6 +16,6 @@ b = np.array([2505, 1880])
 img1 = img[a[1] : b[1], a[0]: b[0]]
 
 # img[top : bottom, left : right]
-#img1 = img[175 : 1875, 230: 2505]
+#img1 = img[255 : 1790, 0: cols]
 
 cv2.imwrite(output_file_path, img1)
