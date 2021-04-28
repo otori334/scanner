bfcc24c81003a183f7fa5c347c29fd8c995da0ea
otori334 <41427185+otori334@users.noreply.github.com>
2021-04-28 14:10:45 +0900

    Update trim.py
    - 二点指定の方法を追加

--- py/trim.py
+++ py/trim.py
@@ -1,12 +1,19 @@
 import cv2
 import sys
+import numpy as np
 
 input_file_path = sys.argv[1]
 output_file_path = sys.argv[2]
 
 img = cv2.imread(input_file_path)
 
+# 左上
+a = np.array([230, 175])
+# 右下
+b = np.array([2505, 1880])
+img1 = img[a[1] : b[1], a[0]: b[0]]
+
 # img[top : bottom, left : right]
-img1 = img[175 : 1875, 230: 2505]
+#img1 = img[175 : 1875, 230: 2505]
 
 cv2.imwrite(output_file_path, img1)
