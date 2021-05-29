79f7382c49f67039ef40168170d8d9273bff3c0c
otori334 <41427185+otori334@users.noreply.github.com>
2021-05-29 13:24:52 +0900

    Update rule_eraser.py
    ROI抽出を導入

--- py/rule_eraser.py
+++ py/rule_eraser.py
@@ -1,7 +1,4 @@
-# http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
-# https://emotionexplorer.blog.fc2.com/blog-entry-181.html
 # 罫線だけ消すやつ
-
 import cv2
 import sys
 import numpy as np
@@ -9,49 +6,70 @@ import numpy as np
 input_file_path = sys.argv[1]
 output_file_path = sys.argv[2]
 
-img = cv2.imread(input_file_path, 0)
+img1 = cv2.imread(input_file_path, 0)
 
 # 大雑把に罫線を消す
-# この段階で消えてしまう横線と消えないノイズは現時点では以降の処理で解決できない
-# この問題はハフ変換などの輪郭検出で克服できる可能性がある
 kernel = np.ones((4, 1), np.uint8)
-closing1 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+closing1 = cv2.morphologyEx(img1, cv2.MORPH_CLOSE, kernel)
 
-# 文字領域を横に伸ばす
-kernel = np.ones((10, 60), np.uint8)
-erosion1 = cv2.erode(closing1, kernel, iterations = 1)
+# 除いた罫線
+# https://emotionexplorer.blog.fc2.com/blog-entry-181.html
+bitwise1 = cv2.bitwise_xor(img1, closing1)
 
-# 余白優先
-or1 = cv2.bitwise_or(img, erosion1)
+# ブランク画像を作成
+rows, cols = img1.shape
+blank = np.zeros((rows, cols, 3), np.uint8)
+blank = cv2.cvtColor(blank, cv2.COLOR_BGR2GRAY)
+
+#←全ゼロデータに255を足してホワイトにする
+blank += 255
+
+# https://kyudy.hatenablog.com/entry/2019/10/26/141330
+thresh_spike = cols / 4
+vp = np.sum((bitwise1 != 0).astype(np.uint8), axis=1)
+loc_y_spike = np.where(vp > thresh_spike)
+line1 = blank.copy()
+
+# ブランク画像に線を描画する
+for y in loc_y_spike[0]:
+    line_color = (0, 0, 0) # black
+    cv2.line(line1, (0, y), (cols, y), line_color, thickness=1)
+
+# 線の歪みを考慮して線のシルエットを大きくする
+kernel = np.ones((10,10),np.uint8)
+line1 = cv2.erode(line1,kernel,iterations = 2)
+
+# 罫線の周囲を切り取る
+bitwise2 = cv2.bitwise_or(img1, line1)
+
+# 大雑把に罫線を消す
+kernel = np.ones((4,1),np.uint8)
+closing2 = cv2.morphologyEx(bitwise2, cv2.MORPH_CLOSE, kernel)
+
+# 黒をつなげる
+kernel = np.ones((10, 150), np.uint8) 
+opening1 = cv2.morphologyEx(closing2, cv2.MORPH_OPEN, kernel)
+
+# 黒を太くする
+kernel = np.ones((5,1),np.uint8)
+erosion1 = cv2.erode(opening1, kernel, iterations = 1)
 
 # 大雑把に罫線を消す
 kernel = np.ones((3,1),np.uint8)
-closing2 = cv2.morphologyEx(or1, cv2.MORPH_CLOSE, kernel)
+closing3 = cv2.morphologyEx(bitwise2, cv2.MORPH_CLOSE, kernel)
 
-# 文字のシルエットを大きくする
-kernel = np.ones((7, 7), np.uint8)
-erosion2 = cv2.erode(closing2, kernel, iterations = 1)
+# 白黒反転
+line2 = 255 - line1
 
-# 横伸びの文字領域と大きくした文字のシルエットが重複する領域を求める（余白優先で黒を足す）
-bitwise = cv2.bitwise_or(erosion1, erosion2)
-
-# 余白優先
-or2 = cv2.bitwise_or(img, bitwise)
-
-# 除いた罫線
-# xor = cv2.bitwise_xor(img, or2)
+# 合成
+bitwise3 = cv2.bitwise_or(closing3, erosion1)
+bitwise4 = cv2.bitwise_or(img1, line2)
+bitwise5 = cv2.bitwise_and(bitwise3, bitwise4)
 
 """
-# cv2.imshow("xor", xor)
-cv2.imshow("or2", or2)
-cv2.imshow("bitwise", bitwise)
-cv2.imshow("erosion2", erosion2)
-cv2.imshow("closing2", closing2)
-cv2.imshow("or1", or1)
-cv2.imshow("erosion1", erosion1)
-cv2.imshow("closing1", closing1)
+cv2.imshow("bitwise5", bitwise5)
 cv2.imshow("img", img)
 cv2.waitKey(0)
 """
 
-cv2.imwrite(output_file_path, or2)
+cv2.imwrite(output_file_path, bitwise5)
