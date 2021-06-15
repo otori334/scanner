b36d16292d8dd1e0b1f852480d4e2578abde98d5
otori334 <41427185+otori334@users.noreply.github.com>
2021-06-15 16:45:14 +0900

    Update graphics.py

--- py/graphics.py
+++ py/graphics.py
@@ -6,10 +6,23 @@ output_file_path = sys.argv[2]
 
 img = cv2.imread(input_file_path)
 rows,cols,channels = img.shape
-frame = 0
+frame = 60
 color = (255, 255, 255)
-cv2.rectangle(img, (frame, frame), (cols - frame, rows - frame), color, thickness=15)
-#cv2.rectangle(img, (0, round(rows * 0.965)), (cols, rows), color, thickness=-1)
-#cv2.rectangle(img, (0, 0), (cols, round(rows * 0.02)), color, thickness=-1)
+
+#cv2.rectangle(img, (frame, frame), (cols - frame, rows - frame), color, thickness=15)
+# 一番下を白くする
+cv2.rectangle(img, (0, round(rows * 0.99)), (cols, rows), color, thickness=-1)
+
+# 一番上を白くする
+cv2.rectangle(img, (0, 0), (cols, round(rows * 0.02)), color, thickness=-1)
+
+# 一番左を白くする
+cv2.rectangle(img, (0, 0), (round(cols * 0.05), rows), color, thickness=-1)
+
+# 一番右を白くする
+cv2.rectangle(img, (round(cols * 0.95), 0), (cols, rows), color, thickness=-1)
+
+# 左上を白くする
+#cv2.rectangle(img, (0, 0), (950, 297), color, thickness=-1)
 
 cv2.imwrite(output_file_path, img)
