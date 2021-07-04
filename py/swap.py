d77467a4404116665813157e03e2daa77b94fae7
otori334 <41427185+otori334@users.noreply.github.com>
2021-07-05 04:24:18 +0900

    Create swap.py

---
+++ py/swap.py
@@ -0,0 +1,16 @@
+# 分割するやつ2
+import cv2
+import sys
+import numpy as np
+
+input_file_path = sys.argv[1]
+output_file_path = sys.argv[2]
+swap = sys.argv[3]
+
+img = cv2.imread(input_file_path, 0)
+
+L = img[:,:swap]
+R = img[:,swap:]
+RL = cv2.hconcat([R, L])
+
+cv2.imwrite(output_file_path, RL)
